3725cd5782cc2c88abb5c9f4c5b44eeb8ccc4bee
elisechldn <elise.chaldjian@gmail.com>
2025-01-13 16:02:34 +0100

    Repo initialized to work with Spring Boot

---
+++ src/test/java/org/wildcodeschool/MyBlog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildcodeschool.MyBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
